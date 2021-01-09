72fde514f7ef51b3cb10a8afab7a4a7f1096b224
Arpit Nema <arpitnema150@gmail.com>
2021-01-09 11:02:06 +0530

    Scond Question

---
+++ hollow_rectangle_pattern.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Print a hollow Rectangle.
+    eg ->
+         *****
+         *   *
+         *   *
+         *   *
+         *****
+*/
+
+int main(){ 
+
+   /* code */
+    int row,col;
+    // getting User input
+    cin>>row>>col;
+    // printing pattern
+
+    for (int i = 1; i <= row; i++){
+        for (int j = 1; j <= col; j++){
+            if(i==1||i==row||j==1||j==col)cout<<"*";
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    /*
+        input - 5 6 
+        output-  
+                ******
+                *    *
+                *    *
+                *    *
+                ****** 
+    */
+    
+
+    return 0;
+}
